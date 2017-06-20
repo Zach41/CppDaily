d914f53badca19f14a95b30db9895a7ace6bbc09
Zach <zach_41@163.com>
2017-06-20 10:31:33 +0800

    thread pool

--- thread_pool/threadpool.h
+++ thread_pool/threadpool.h
@@ -2,28 +2,27 @@
 #define THREAD_POOL_H
 
 #include <vector>
-#include <mutex>
 #include <thread>
-#include <queue>
 #include <functional>
-#include <future>
+#include <mutex>
+#include <queue>
 #include <type_traits>
+#include <future>
 #include <utility>
 #include <memory>
 
 namespace pool {
     class ThreadPool {
     public:
-        ThreadPool(size_t);
+        explicit ThreadPool(size_t);
         template <typename F, typename... Args>
         auto enqueue(F&& f, Args&&... args)
-            -> std::future<typename std::result_of<F(Args...)>::type>;
-        ~ThreadPool();  
+             -> std::future<typename std::result_of<F(Args...)>::type>;
+        ~ThreadPool();
     private:
+        std::mutex queue_mutex;
         std::vector<std::thread> workers;
         std::queue<std::function<void()>> tasks;
-        
-        std::mutex queue_mutex;
         std::condition_variable condition;
         bool stop;
     };
@@ -35,10 +34,9 @@ namespace pool {
                 [this] {
                     while (true) {
                         std::function<void()> task;
-
                         {
                             std::unique_lock<std::mutex> lock(this -> queue_mutex);
-                            this -> condition.wait(lock,
+                            this -> condition.wait(lock, 
                                 [this] { return this -> stop || !this -> tasks.empty(); });
                             if (this -> stop && this -> tasks.empty())
                                 return;
@@ -54,28 +52,28 @@ namespace pool {
 
     template <typename F, typename... Args>
     auto ThreadPool::enqueue(F&& f, Args&&... args)
-         -> std::future<typename std::result_of<F(Args...)>::type>
+        -> std::future<typename std::result_of<F(Args...)>::type>
     {
         using return_type = typename std::result_of<F(Args...)>::type;
-
+        
         auto task = std::make_shared<std::packaged_task<return_type()>>(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        std::future<return_type> ret_future = task -> get_future();
 
+        auto ret_future = task -> get_future();
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-
             if (stop)
-                throw std::runtime_error("enqueue on stopped ThreadPool");
-            
-            tasks.emplace([task]{ (*task)(); });
+                throw std::runtime_error("enqueue on stopped threadpool");
+            tasks.emplace(
+                [task] { (*task)(); }
+            );
+            condition.notify_one();
         }
-        condition.notify_one();
         return ret_future;
     }
 
-    inline ThreadPool::~ThreadPool()
+    ThreadPool::~ThreadPool()
     {
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
